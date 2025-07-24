7efdf2f65164b088780cc894dcef0fe2c854e98d
sayalipatil1528 <sayalisureshpatil91@gmail.com>
2025-07-24 12:42:35 -0400

    Add files via upload

---
+++ C Programming/Assignment 0/1) Addition of two integers.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int a=20,b=30,c;
+	c=a+b;
+	printf("Addition of %d and %d is %d",a,b,c);
+}
---
+++ C Programming/Assignment 0/8) Perimeter Of Rectangle.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int length=10,width=5,perimeter;
+	perimeter=2*(length*width);
+	printf("Perimeter of reactangle is %d",perimeter);
+}

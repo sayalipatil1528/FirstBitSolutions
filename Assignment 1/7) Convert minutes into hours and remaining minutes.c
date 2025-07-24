2bcf4d9071a4018ff9a7818c234efa57bae1ba78
sayalipatil1528 <sayalisureshpatil91@gmail.com>
2025-07-24 12:45:33 -0400

    Delete Assignment 1 directory

--- Assignment 1/7) Convert minutes into hours and remaining minutes.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<conio.h>
-void main()
-{
-	int min=220,hours,remmin;
-	hours=min/60;
-	remmin=min%60;
-	printf("Hours is %d and Remaining Minutes is %d",hours,remmin);
-}

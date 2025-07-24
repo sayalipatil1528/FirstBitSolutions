479bd4140add04487fcb8433aa82d67db1d1ce43
sayalipatil1528 <sayalisureshpatil91@gmail.com>
2025-07-24 13:05:03 -0400

    Add files via upload

---
+++ C Programming/Assignment 0/FirstbitSolutions/C Programming/Assignment 0/3) Temperature From Celcius To Fahrenheit.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int C=80,F;
+	F=(C*9/5)+32;
+	printf("Temperature from Celcius to Fahrenheit is %d ",F);
+}
---
+++ C Programming/Assignment 0/FirstbitSolutions/C Programming/Assignment 0/6) Square And Cube Of Given Number.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+	int No=5,Square,Cube;
+	Square=No*No;
+	Cube=No*No*No;
+	printf("Square of %d is %d \n",No,Square);
+	printf ("Cube of %d is %d",No,Cube);
+}
